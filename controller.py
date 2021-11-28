1063f51e78d294d42006ea8b3d96e5fce14918f2
Pakarat Kongpaisan <pakarat@MacBook-Pro-khxng-Pakarat.local>
2021-11-28 18:51:51 +0700

    add controller

--- controller.py
+++ controller.py
@@ -6,31 +6,37 @@ from config import OPENAPI_AUTOGEN_DIR, DB_HOST, DB_USER, DB_PASSWD, DB_NAME
 sys.path.append(OPENAPI_AUTOGEN_DIR)
 from openapi_server import models
 
-db = mysql.connect(host=DB_HOST,
-                   user=DB_USER,
-                   passwd=DB_PASSWD,
-                   db=DB_NAME)
+db = mysql.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWD, db=DB_NAME)
 
 
 def get_lat_long(date):
     cs = db.cursor()
-    cs.execute("""
+    cs.execute(
+        """
         SELECT timestamp, latitude, longitude
         FROM latlon
         WHERE DATE(timestamp) =%s
-        """, [date])
-    result = [models.LatLong(timestamp, latitude, longitude) for timestamp, latitude, longitude in cs.fetchall()]
+        """,
+        [date],
+    )
+    result = [
+        models.LatLong(timestamp, latitude, longitude)
+        for timestamp, latitude, longitude in cs.fetchall()
+    ]
     cs.close()
     return result
 
 
 def get_pm(date):
     cs = db.cursor()
-    cs.execute("""
+    cs.execute(
+        """
         SELECT timestamp, pm25
         FROM pm25andprovince
         WHERE DATE(timestamp) =%s
-        """, [date])
+        """,
+        [date],
+    )
     result = [models.PM25(timestamp, pm25) for timestamp, pm25 in cs.fetchall()]
     cs.close()
     return result
@@ -38,11 +44,14 @@ def get_pm(date):
 
 def get_province(date):
     cs = db.cursor()
-    cs.execute("""
+    cs.execute(
+        """
         SELECT timestamp, province
         FROM pm25andprovince
         WHERE DATE(timestamp) =%s
-        """, [date])
+        """,
+        [date],
+    )
     result = [models.PM25(timestamp, province) for timestamp, province in cs.fetchall()]
     cs.close()
     return result
@@ -50,11 +59,14 @@ def get_province(date):
 
 def get_population(province):
     cs = db.cursor()
-    cs.execute("""
+    cs.execute(
+        """
         SELECT province, population
         FROM population
         WHERE province =%s
-        """, [province])
+        """,
+        [province],
+    )
     result = cs.fetchone()
     cs.close()
     if result:
@@ -66,34 +78,85 @@ def get_population(province):
 
 def get_covid19(date):
     cs = db.cursor()
-    cs.execute("""
+    cs.execute(
+        """
         SELECT timestamp, newcovid, totalcovid
         FROM covid19
         WHERE DATE(timestamp) =%s
-        """, [date])
-    result = [models.Covid19(timestamp, newcovid, totalcovid) for timestamp, newcovid, totalcovid in cs.fetchall()]
+        """,
+        [date],
+    )
+    result = [
+        models.Covid19(timestamp, newcovid, totalcovid)
+        for timestamp, newcovid, totalcovid in cs.fetchall()
+    ]
     cs.close()
-    return result    
-    
+    return result
+
 
 def get_provinces_population():
     cs = db.cursor()
-    cs.execute("""
+    cs.execute(
+        """
         SELECT province, population
         FROM population
-        """)
-    result = [models.ProvincePopulation(province, population) for province, population in cs.fetchall()]
+        """
+    )
+    result = [
+        models.ProvincePopulation(province, population)
+        for province, population in cs.fetchall()
+    ]
     cs.close()
-    return result    
+    return result
 
 
 def get_covid19_trend():
     cs = db.cursor()
-    cs.execute("""
+    cs.execute(
+        """
         SELECT p.province, population, newcovid
         FROM population p
         INNER JOIN covid19 c ON p.province=c.province
-        """)
-    result = [models.CovidTrend(province, population, newcovid) for province, population, newcovid in cs.fetchall()]
+        """
+    )
+    result = [
+        models.CovidTrend(province, population, newcovid)
+        for province, population, newcovid in cs.fetchall()
+    ]
     cs.close()
-    return result 
+    return result
+
+
+def get_sum_covid19():
+    cs = db.cursor()
+    cs.execute(
+        """
+        SELECT p.province, population, SUM(newcovid) as sum
+        FROM population p
+        INNER JOIN covid19 c ON p.province=c.province
+        GROUP BY province, population
+        """
+    )
+    result = [
+        models.SumCovid(province, population, sum)
+        for province, population, sum in cs.fetchall()
+    ]
+    cs.close()
+    return result
+
+
+def get_pm_trend():
+    cs = db.cursor()
+    cs.execute(
+        """
+        SELECT p.province, population, pm25
+        FROM population p
+        INNER JOIN pm25andprovince pm ON p.province=pm.province
+        """
+    )
+    result = [
+        models.PmTrend(province, population, pm25)
+        for province, population, pm25 in cs.fetchall()
+    ]
+    cs.close()
+    return result
